3df0413a62ea3737ade0652eb772c7ccbfaf3309
CaMiMtoto <jeanpaulcami@live.com>
2019-11-27 11:17:34 +0200

    add role query

---
+++ database/insert_roles.sql
@@ -0,0 +1,4 @@
+INSERT INTO roles (id, name, display, created_at, updated_at) VALUES (1, 'admin', 'Administrator', '2019-11-27 11:12:46', '2019-11-27 11:12:46');
+INSERT INTO roles (id, name, display, created_at, updated_at) VALUES (2, 'cashier', 'Cashier', '2019-11-27 11:12:46', '2019-11-27 11:12:46');
+INSERT INTO roles (id, name, display, created_at, updated_at) VALUES (3, 'keeper', 'Store Keeper', '2019-11-27 11:12:46', '2019-11-27 11:12:46');
+INSERT INTO roles (id, name, display, created_at, updated_at) VALUES (4, 'manager', 'Manager', '2019-11-27 11:12:46', '2019-11-27 11:12:46');
